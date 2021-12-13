b8d7aa7034bab5d1285ea6fc7384301a9598ce4b
oscarholt <38869989+oscarholt@users.noreply.github.com>
2021-12-14 12:02:35 +1300

    add countdown timer

--- src/App.js
+++ src/App.js
@@ -6,17 +6,39 @@ const SECONDS = 60
 
 function App() {
   const [words, setWords] = useState([])
+  const [countDown, setCountDown] = useState(SECONDS)
 
   useEffect(() => {
+    //Sets our words state to be an array of generated words using the random-words package
     setWords(generateWords())
   }, [])
 
   function generateWords() {
+    // Returns an array of random words
     return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords())
+  }
+
+  function start() {
+    setInterval(() => {
+      setCountDown((prevCountDown) => prevCountDown - 1)
+    } , 1000)
   }
 
   return (
     <div className='App'>
+      <div className="section">
+        <div className="is-size-1 has-text-centered has-text-primary">
+          <h2>{countDown}</h2>
+        </div>
+      </div>
+      <div className="control is-expanded section">
+        <input type="text"className="input"/>
+      </div>
+      <div className="section">
+        <button className="button is-info is-fullwidth" onClick={start}>
+          Start
+        </button>
+      </div>
       <div className='section'>
         <div className='card'>
           <div className='card-content'>
